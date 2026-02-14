c6ad2aaa775d34d90b65d5a8f4d9b9cf986799d7
JCUK2025 <johncurtis.email@gmail.com>
2026-02-14 13:06:01 +0000

    Create src/App.js with the DeliveryAreasManager component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DeliveryAreasManager from './DeliveryAreasManager';
+
+function App() {
+  return (
+    <div>
+      <h1>Delivery Areas Manager</h1>
+      <DeliveryAreasManager />
+    </div>
+  );
+}
+
+export default App;
